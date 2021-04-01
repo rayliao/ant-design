07e8900a7dffbef954547cd06890a1687ba2925b
WingRay Liao <rayliao1989@gmail.com>
2021-04-01 16:30:25 +0800

    Create km_KH.tsx

---
+++ components/time-picker/locale/km_KH.tsx
@@ -0,0 +1,8 @@
+import { TimePickerLocale } from '../index';
+
+const locale: TimePickerLocale = {
+  placeholder: 'ជ្រើសរើសពេលវេលា',
+  rangePlaceholder: ['ពេលវេលាចាប់ផ្តើម', 'ពេលវេលាបញ្ចប់'],
+};
+
+export default locale;
